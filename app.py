2f00ecf6c34a261c6ad44c4cb8a82fc5319f04cf
mclarentyper <hiroyuki.uchida@pwc.com>
2022-02-12 18:58:41 +0900

    update app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from operator import length_hint
+from random import choice
 import string
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -9,49 +11,79 @@ import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, DateTime, Float
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select
 import datetime
 
-# write data to sqlite
 
-db_path = r"./quality_db.sqlite3"
-engine = sqlalchemy.create_engine(f'sqlite:///{db_path}', echo=True)
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-session = Session()
-class Quality(Base):
+def main():
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    kanri = Column(String(length=255))
-    created_at = Column(DateTime, default=datetime.datetime.now)
-    updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
-    kako_no = Column(Integer)
-    hinban = Column(String(length = 255))
-    result = Column(Float)
+    db_path = r"./quality_db.sqlite3"
+    engine = sqlalchemy.create_engine(f'sqlite:///{db_path}', echo=True)
+    Base = declarative_base()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-    __tablename__ = 't_quality'
+    class Quality(Base):
 
-Base.metadata.create_all(bind=engine)
+        id = Column(Integer, primary_key=True, autoincrement=True)
+        kanri = Column(String(length=255))
+        created_at = Column(DateTime, default=datetime.datetime.now)
+        updated_at = Column(DateTime, default=datetime.datetime.now, onupdate=datetime.datetime.now)
+        kako_no = Column(Integer)
+        hinban = Column(String(length = 255))
+        result = Column(Float)
 
-st.title('工程能力管理')
+        __tablename__ = 't_quality'
 
-kanri = st.radio(
-     "初物管理",
-     ('初物', '終物', 'その他'))
+    Base.metadata.create_all(bind=engine)
 
-number = st.number_input('加工済みロット数', step=1)
-st.write('加工数 ：', number*25 )
+    menu = ["実績登録","データチェック"]
+    choice = st.sidebar.selectbox("メニュー",menu)
 
-code = st.text_input('看板バーコード')
-st.write('品番 ：', code)
+    
+    if choice == "実績登録":
+        st.title('工程能力管理')
 
-# 品番
+        kanri = st.radio(
+            "初物管理",
+            ('初物', '終物', 'その他'))
 
-result = st.number_input('被覆剥取長[mm]')
-st.write('被覆剥取長 ：', result )
+        number = st.number_input('加工済みロット数', step=1)
+        st.write('加工数 ：', number*25 )
 
-if st.button('登録'):
-    Data = Quality(kanri=kanri,kako_no=number,hinban=code,result=result)
-    session.add(Data)
-    session.commit()
+        code = st.text_input('看板バーコード')
+        st.write('品番 ：', code)
 
-    st.write(f'{code}の{kanri}実績値を登録しました。')
+        # 品番
+
+        result = st.number_input('被覆剥取長[mm]')
+        st.write('被覆剥取長 ：', result )
+
+        if st.button('登録'):
+            Data = Quality(kanri=kanri,kako_no=number,hinban=code,result=result)
+            session.add(Data)
+            session.commit()
+
+            st.write(f'{code}の{kanri}実績値を登録しました。')
+        
+    elif choice == "データチェック":
+        st.title('データチェック')
+
+        # select
+        sql_statement = (
+            select([
+                Quality.id,
+                Quality.hinban,
+                Quality.kako_no,
+                Quality.created_at,
+                Quality.result
+            ])
+            
+        )
+    df = pd.read_sql_query(sql=sql_statement, con=engine)
+
+    st.write(df)
+
+
+if __name__ == '__main__':
+    main()
